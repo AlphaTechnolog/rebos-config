87bc8c8a4cfc065526d53ca02c41f916d2893f81
Gabriel Guerra <gfranklings@gmail.com>
2024-10-07 17:45:25 -0400

    feat: Packaging gnome (gnome-light), so will probably need some tweaks after build

--- hooks/common/utils.sh
+++ hooks/common/utils.sh
@@ -1,3 +1,7 @@
+# This file is probably gonna cause rebos config check to fail with a single
+# warning at least, by saying that common or common/utils.sh idk does not matches
+# with any manager so it won't run this, which is fine and can be skipped.
+
 log_fatal() {
 	rebos api echo fatal "$@"
 }
